36f5f4ea530f8560afa09ded15f84deb2fe981f8
Michal Rackiewicz <michael.rackiewicz@gmail.com>
2023-09-28 01:03:09 +0200

    fixed partial user input and NMF almost working

--- code/global_data.R
+++ code/global_data.R
@@ -1,3 +1,5 @@
+# dfs for basic recommender ----
+
 # pivot into wide format and replace NAs with 0 values
 rating_matrix <- ratings_df %>% 
   pivot_wider(names_from = movieId, values_from = rating) %>% 
@@ -12,27 +14,48 @@ movies_rated <- rating_matrix %>%
   mutate(movieId = as.integer(movieId))
 
 # load movie annotations
-movie_df <-  read.csv("C:/r_projects/shiny_movie_recommender/Shiny_Movie_Recommender/data/movies.csv")
-movie_ref <- movie_df %>% 
-  mutate(genres =  str_split(genres, pattern = "\\|")) %>% 
+movie_df <-  read.csv(".data/movies.csv")
+
+movie_ref_table <- movie_df %>% 
+  # transform genres into separate columns
+  dummy_columns(select_columns = "genres", split = "\\|", 
+                remove_selected_columns = TRUE) %>%
+  rename_with(., ~ gsub("genres_", "", .), .cols = starts_with("genres")) %>%
+  # remove all entries withou genre
+  filter("(no genres listed)" != 1) %>% 
+  select(!"(no genres listed)") %>% 
+  # add movie ratings
   left_join(movies_rated, by = "movieId") %>% 
+  # remove entries without ratings
   filter(!is.na(rating))
 
-
+glimpse(movie_ref_table)
 # extract the genres into a list for user selection
 genre_list <- movie_ref %>% 
   pull(genres) %>% 
   flatten_chr() %>% 
   unique()
 
+
 # remove "no genres listed"
 genre_list <- genre_list[-20]
 
+movie_ref %>% 
+  rowwise() %>% 
+  summarise(len = length(genres)) %>% 
+  ungroup() %>% 
+  max()
+# create dummy variables for movie genres?
+
 # load ratings 
-ratings_df <- read.csv("C:/r_projects/shiny_movie_recommender/Shiny_Movie_Recommender/data/ratings.csv")
+ratings_df <- read.csv("./data/ratings.csv")
 # remove timestamp and filter for movies with at least 10 valid values
 ratings_df <- ratings_df %>% 
   select(-timestamp) %>% 
   group_by(movieId) %>% 
   filter(sum(!is.na(rating)) >= 10)%>% 
   ungroup()
+
+
+
+
--- code/recommender.R
+++ code/recommender.R
@@ -1,46 +1,21 @@
-# install.packages(c"(NMF", "proxy"))
-
-library(tidyverse)
-library(shiny)
-library(proxy)
-library(NMF)
-
-
-
-
-
-# create user interface
+# cuser interface ----
 ui <- fluidPage(
-  # first select a genre
+  # select a movie genre
   selectInput("genre", "Select a movie genre", choices = genre_list), 
-  actionButton("submit_genre", "Submit Genre"),
+  actionButton("submit_genre", "Submit genre"),
   conditionalPanel(
     condition = "output.showRatingUI",
     uiOutput("rating_ui"),
-    selectInput("method", "Select a recommendation method", choices = c("nmf", "random", "vector", "cluster")),
+    #  allow user to select recommender method
+    selectInput("method", "Select a recommendation method", 
+                choices = c("nmf", "random", "vector", "cluster")),
     actionButton("submit_recommendations", "Get recommendations"),
+    # display movie recommendations
     tableOutput("recommendations")
   )
 )
 
-# server <- function(input, output, session) {
-#   output$rating_ui <- renderUI({
-#     genre_selected <- input$genre
-#     #  select popular movies in the selected genres
-#     popular_movies <- movie_ref %>%
-#       # filter movies for selected genre 
-#       filter(map_lgl(genres, ~genre_selected%in% genres)) %>%
-#       # select the top 5 highest-ranking
-#       arrange(desc(rating)) %>% 
-#       head(5) %>%
-#       pull(title)
-#     
-#     movie_rating_inputs <- lapply(popular_movies, function(movie) {
-#       numericInput(movie, label = movie, value = NA, min = 1.0, max = 5.0)
-#     })
-#     do.call(tagList, movie_rating_inputs)
-#   })
-#   
+# server ----
 server <- function(input, output, session) {
     
   showRatingUI <- reactiveVal(FALSE)
@@ -59,14 +34,15 @@ server <- function(input, output, session) {
     if(showRatingUI()) {
       genre_selected <- input$genre
       # select the five most-popular movies from the user-selected genre 
-      popular_movies <- movie_ref %>%
-        filter(map_lgl(genres, ~genre_selected %in% .)) %>%
+      popular_movies_to_rate <- movie_ref_table %>%
+        # filter the respective genre-column based on input
+        filter(.data[[genre_selected]] == 1) %>% 
         arrange(desc(rating)) %>% 
-        head(5) %>%
+        head(10) %>%
         pull(title)
         
+      movie_rating_inputs <- lapply(popular_movies_to_rate, function(movie) {
       # make movie titles HTML-conform by removing spaces
-      movie_rating_inputs <- lapply(popular_movies, function(movie) {
         sanitized_movie_id <- gsub("[^a-zA-Z0-9]", "_", movie)
         numericInput(sanitized_movie_id, label = movie, value = NA, min = 1.0, max = 5.0)
       })
@@ -74,36 +50,84 @@ server <- function(input, output, session) {
       do.call(tagList, movie_rating_inputs)
     }
   })
-  observeEvent(input$submit_recommendatyions, {
-    input_data <- sapply(names(input)[-1:-4], function(i) input[[i]])
-    user_vector <- as.numeric(input_data)
-    names(user_vector) <- names(input)[-1:-4]
+  # generate recommendations after user prompt
+  observeEvent(input$submit_recommendations, {
+    # store user input 
+    input_data <- sapply(names(input), function(i) input[[i]])
     
-    # create empty vecotr for recommendations
+    # retrieve the movies and 
+    user_rated_movies <- movie_ref_table %>%
+      # filter the respective genre-column based on input
+      filter(.data[[genre_selected]] == 1) %>% 
+      arrange(desc(rating)) %>% 
+      head(10) %>% 
+      select(movieId, title)
+    
+    # set default rating to 0
+    user_ratings <- tibble(title = user_rated_movies$title, 
+                           rating = 0,
+                           stringsAsFactors = FALSE)
+    
+    # update ratings according to user inputs
+    # create a logical mask indicating which of the popular movies the user has rated
+    user_rating_mask <- user_ratings$title %in% names(input_data)
+    # override default rating with user rating
+    user_ratings$rating[user_rating_mask] <- input_data
+    
+    # merge tables to attach movieID for recommender algorithms
+    user_ratings <- user_ratings %>% 
+      left_join(user_rated_movies, by = title)
+    
+    # create empty vector for recommendations
     recommendations <- NULL
     
-    # conditional recommendations
+    # conditional recommendations ----
+    
+    # NMF-based recommendation ----
     if(input$method == "nmf") {
-      # NMF-based recommendation
-      nmf_model <- nmf(rating_matrix, 2)
-      W <- basis(nmf_model)
-      H <- coef(nmf_model)
-      new_user <- as.matrix(user_vector) %*% solve(W)
-      estimated_rating <- new_user %*% H
-      recommendations <- names(sort(estimated_rating, decreasing = TRUE))[1:5]
+      # create empty vector for new user (NMF does not accept NAs)
+      new_user_ratings <- rep(0, ncol(rating_matrix))
+      new_user_ratings[user_rated_movies$movieId] <- user_rated_movies$rating
       
+      # update rating matrix by attaching ratings by the user
+      rating_matrix <- rbind(rating_matrix, new_user_ratings)
+      
+      # create nmf-model
+      nmf_model <- nmf(rating_matrix, 2, method = "brunet", seed = 123)
+      # extract user-feature matrix
+      W <- basis(nmf_model)
+      # extract feature_movie matrix
+      H <- coef(nmf_model)
+      
+      # reconstruct rating matrix
+      approx_rating_matrix <- W %*% H
+      
+      # predict recommendations for the user
+      new_user_features <- new_user_ratings %*% solve(W)
+      # predict movie ratings by the user
+      new_user_predicted_ratings <- new_user_features %*% H 
+      
+      # select top5-rated movie ids as predicted
+      recommended_movie_ids <- order(new_user_predicted_ratings, decreasing = TRUE)[1:5]
+      
+      # retrieve movie title from reference table
+      recommended_movies <- movie_ref_table %>% 
+        filter(movieId %in% recommended_movie_ids) %>% 
+        select(title)
+      
+      
+    # Random recommendation
     } else if(input$method == "random") {
-      # Random recommendation
       recommendations <- sample(colnames(rating_matrix), 5)
       
+    # Cosine Similarity-based recommendation
     } else if(input$method == "vector") {
-      # Cosine Similarity- based recommendation
       sim <- sim2(as.matrix(rating_matrix), method = "cosine")
       estimated_rating <- as.matrix(user_vector) %*% sim
       recommendations <- names(sort(estimated_rating, decreasing = TRUE))[1:5]
       
+    # kNN -based recommendations
     } else if(input$method == "cluster") {
-      # kNN -based recommendations
       sim <- sim2(as.matrix(rating_matrix), method = "euclidean")
       nn <- knn(sim, k = 5)
       estimated_rating <- rowMeans(as.matrix(nn), na.rm = TRUE)
