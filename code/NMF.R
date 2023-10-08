1537b56b423daea949b0ebe416f7fbe4ea061840
Michal Rackiewicz <michael.rackiewicz@gmail.com>
2023-10-08 23:02:46 +0200

    more NMF df dims checks

--- code/NMF.R
+++ code/NMF.R
@@ -14,7 +14,23 @@ print(paste("Length of user_rated_movies$movieId:", length(user_rated_movies$mov
 print(paste("Length of user_rated_movies$title:", length(user_rated_movies$title)))
 print(paste("Length of user_rated_movies$rating (if exists):", length(user_rated_movies$rating)))
 
+str(user_rated_movies)
+
 # check dimensions of rating_matrix
 print(dim(rating_matrix))
 
-# 
+# Simulated user ratings for 10 movies
+simulated_user_input <- runif(10, 1, 5) 
+
+new_user_ratings <- rep(0, ncol(rating_matrix))
+new_user_ratings[user_rated_movies$movieId] <- simulated_user_input
+
+print(paste("Length of new_user_ratings:", length(new_user_ratings)))
+
+print(paste("Number of columns in rating_matrix:", ncol(rating_matrix)))
+print(paste("Length of new_user_ratings:", length(new_user_ratings)))
+
+str(rating_matrix)
+str(new_user_ratings)
+
+new_user_ratings[user_rated_movies$movieId]
